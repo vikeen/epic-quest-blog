354a72c8249614e1f86e72adc642c0165ab6eb14
John Rake <john.rake12@gmail.com>
2019-10-22 22:02:05 -0500

    add theme javascripts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-//= link bootstrap.css
-//= link font-awesome.min.css
-//= link owl.theme.css
-//= link owl.carousel.css
-//= link reset.css
-//= link mat-vcard.theme.css
+//= link_directory ../../../vendor/assets/javascripts .js
+
+//= link_directory ../../../vendor/assets/stylesheets .css
 //= link colors/blue.css
