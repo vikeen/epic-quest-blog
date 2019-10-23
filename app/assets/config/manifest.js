86643c970219e38fa3bb786460c7e6529c4ca63b
John Rake <john.rake12@gmail.com>
2019-10-22 19:43:39 -0500

    add default theme css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= link bootstrap.css
+//= link owl.theme.css
+//= link owl.carousel.css
+//= link reset.css
+//= link mat-vcard.theme.css
