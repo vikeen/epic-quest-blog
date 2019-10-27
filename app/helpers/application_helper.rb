1f4cbd5b059d29effa36c83b12d0e081716953ce
John Rake <john.rake12@gmail.com>
2019-10-27 13:40:42 -0500

    refactor personal info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
     def facebook_url
         return "https://www.facebook.com/john.rake.37"
     end
+
+    def my_job_title
+        return "Web Developer"
+    end
+
+    def my_full_name
+        return "John Rake"
+    end
+
+    def my_email
+        return "john.rake12@gmail.com"
+    end
+
+    def my_location
+        return "Olathe, KS USA"
+    end
 end
