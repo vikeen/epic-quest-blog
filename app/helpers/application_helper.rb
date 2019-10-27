b11bc6df822680295959ab6a9de56f88f842c730
John Rake <john.rake12@gmail.com>
2019-10-27 12:28:09 -0500

    refactor social links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def linkedin_url
+        return "https://www.linkedin.com/in/john-rake"
+    end
+
+    def facebook_url
+        return "https://www.facebook.com/john.rake.37"
+    end
 end
