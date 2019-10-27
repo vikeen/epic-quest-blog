27b5fc71ec345f1d769e831314864c806c24f102
John Rake <john.rake12@gmail.com>
2019-10-27 12:11:10 -0500

    add email and gravatar to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_165552) do
+ActiveRecord::Schema.define(version: 2019_10_27_163519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_165552) do
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
