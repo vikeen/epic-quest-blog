0e796854eea165d7b4697eba415a567414a77505
John Rake <john.rake12@gmail.com>
2019-10-29 12:21:11 -0500

    require tag name and key

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
     has_many :portfolio_item_tags
     has_many :portfolio_items, through: :portfolio_item_tags
+
+    validates :name, presence: true
+    validates :key, presence: true
 end
