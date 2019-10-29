28b8ecae4f7c9a33cfdd4766944b0ceda9b24c03
John Rake <john.rake12@gmail.com>
2019-10-28 20:23:33 -0500

    update tag model

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_10_28_212128) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.string "key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
